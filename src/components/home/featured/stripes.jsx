93a9c2ef0814f5f86037c56b794264fe43c99708
Rajatsingh94 <rajat94.singh@gmail.com>
2018-08-08 16:26:07 -0400

    Header/Footer/Featured component added

---
+++ src/components/home/featured/stripes.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {easePolyOut} from 'd3-ease';
+import Animate from 'react-move/Animate';
+
+class Stripes extends Component {
+
+    showStripes = () =>{
+        return <div>Stripes</div>
+    }
+
+    render() {
+        return (
+            <div classname="featured_stripes">
+                {this.showStripes()}
+            </div>
+        );
+    }
+}
+
+export default Stripes;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './routes';
+import {BrowserRouter} from 'react-router-dom';
+import './Resources/Resources/css/app.css';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes></Routes>
+        </BrowserRouter>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
